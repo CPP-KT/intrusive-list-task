a6469cda142b2c46740955e18589be018805cfe7
334905 <334905@niuitmo.ru>
2023-09-06 14:39:30 +0300

    Size removed and added asymptotics.

--- intrusive_list.h
+++ intrusive_list.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <cstddef>
-
 namespace intrusive {
 struct default_tag;
 
@@ -14,16 +12,20 @@ struct list {
   using const_iterator = void;
 
   // Constructs an empty list.
+  // O(1).
   list() noexcept;
   // Unlinks all the elements from the list.
+  // O(n).
   ~list() noexcept;
 
   list(const list&) noexcept = delete;
   list& operator=(const list&) noexcept = delete;
   // Transfers all the elements from other into *this.
+  // O(1).
   list(list&& other) noexcept;
   // Unlinks all the elements out of this
   //     and transfers all the elements from other to *this.
+  // O(n), where n is this->size() before calling operator=.
   list& operator=(list&& other) noexcept;
 
   iterator begin() noexcept;
@@ -33,37 +35,45 @@ struct list {
 
   // Returns a reference to the first element in the list.
   // Calling front on an empty list causes UB.
+  // O(1).
   T& front() noexcept;
   const T& front() const noexcept;
   // Returns a reference to the last element in the list.
   // Calling back on an empty list causes UB.
+  // O(1).
   T& back() noexcept;
   const T& back() const noexcept;
 
-  std::size_t size() const noexcept;
+  // O(1).
   bool empty() const noexcept;
 
   // Appends exactly value (not copy) to the beginning of the list.
   // If any list contained value before calling push_front,
   //     it gets unlinked out of that list.
+  // O(1).
   void push_front(T& value) noexcept;
   // Appends exactly value (not copy) to the end of the list.
   // If any list contained value before calling push_back,
   //     it gets unlinked out of that list.
+  // O(1).
   void push_back(T& value) noexcept;
   // Unlinks the first element of the list.
   // Calling push_back on an empty list causes UB.
+  // O(1).
   void pop_front() noexcept;
   // Unlinks the last element of the list.
   // Calling pop_back on an empty list causes UB.
+  // O(1).
   void pop_back() noexcept;
 
   // Inserts exactly value (not copy) before pos.
   // Returns an iterator pointing to value.
   // If any list contained value before calling insert,
   //     it gets unlinked out of that list.
+  // O(1).
   iterator insert(const_iterator pos, T& elem) noexcept;
   // Unlinks the element at pos. The iterator pos must not be end().
+  // O(1).
   iterator erase(const_iterator pos) noexcept;
 
   // pos must be an iterator into *this,
@@ -71,6 +81,7 @@ struct list {
   // Transfers the elements in the range [first, last) from other into *this.
   // The elements are inserted before pos.
   // It's allowed for *this and other to be the same object.
+  // O(1).
   void splice(const_iterator pos, list& other, const_iterator first,
               const_iterator last) noexcept;
 };
